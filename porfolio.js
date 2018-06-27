66a6580f56836d73d78c79a0f0650b1f4573d902
bababwii <40634268+bababwii@users.noreply.github.com>
2018-06-27 16:40:20 +0300

    my first website

---
+++ porfolio.js
@@ -0,0 +1,6 @@
+var clickme=document.getElementById("clickme");
+clickme.addEventListener("click", function(){
+  var yes=prompt("what is your name")
+  var yes=prompt ("hi" + name + "hope you are fine?")
+  alert("good to hear that have a blessed day" + name)
+})
